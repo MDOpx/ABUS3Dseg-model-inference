b9738517de5689aef8e065179821b856656da5f0
YoonChiHo <48755220+YoonChiHo@users.noreply.github.com>
2024-07-31 09:10:45 +0900

    Delete code/hybridnet directory

--- code/hybridnet/configuration.py
+++
@@ -1,5 +0,0 @@
-import os
-
-default_num_threads = min(os.cpu_count(), int(os.environ['nnUNet_num_proc'])) if 'nnUNet_def_n_proc' not in os.environ else int(os.environ['nnUNet_def_n_proc'])
-RESAMPLING_SEPARATE_Z_ANISO_THRESHOLD = 3  # determines what threshold to use for resampling the low resolution axis
-# separately (with NN)
